712f9faafad6add3d672b6fd3dcac921ea1b7f5a
DarK <icodergamedev@gmail.com>
2023-08-16 12:41:11 +0200

    use localStorage instead of session so we can reload examples easily

--- dev/editorLoad.ts
+++ dev/editorLoad.ts
@@ -1,8 +1,8 @@
 var loadInterval;
-function changeMapData(data) {
+function changeMapData(data:string) {
     window.loaded = true;
     MapData.data = data;
-    window.sessionStorage.setItem("map",MapData.data);
+    localStorage.setItem("map",MapData.data);
 }
 function doReload() {
     console.error("works");
@@ -16,8 +16,8 @@ function checkLoad() {
         JsonMap(MapData);
     }else if(window.editorData) {
         changeMapData(window.editorData)
-    }else if(sessionStorage.getItem("map")) {
-        changeMapData(sessionStorage.getItem("map"));
+    }else if(localStorage.getItem("map")) {
+        changeMapData(localStorage.getItem("map"));
     }
 }
 function preload() {
@@ -40,18 +40,5 @@ function draw() {
     if(!window.loaded) return;
     if(!window?.player?.update) return; 
     if(!window?.engine?.getActiveScene) return; 
-    clear();
-    background(150, 230, 240);
-    /*-------------PLAYER AND LEVEL DRAWING-----------------*/
-    player.update();
-    //Early Update
-    engine.getActiveScene().earlyUpdate();
-    player.camera();
-    player.checkCollisions();
-    engine.getActiveScene().display();
-    engine.getActiveScene().customDraw(false);
-    player.display();
-    //Late Update
-    engine.getActiveScene().lateUpdate();
-    /*-------------PLAYER AND LEVEL DRAWING-----------------*/
+    engine.draw()
 }
--- docs/editorLoad.js
+++ docs/editorLoad.js
@@ -2,7 +2,7 @@ var loadInterval;
 function changeMapData(data) {
     window.loaded = true;
     MapData.data = data;
-    window.sessionStorage.setItem("map", MapData.data);
+    localStorage.setItem("map", MapData.data);
 }
 function doReload() {
     console.error("works");
@@ -18,8 +18,8 @@ function checkLoad() {
     else if (window.editorData) {
         changeMapData(window.editorData);
     }
-    else if (sessionStorage.getItem("map")) {
-        changeMapData(sessionStorage.getItem("map"));
+    else if (localStorage.getItem("map")) {
+        changeMapData(localStorage.getItem("map"));
     }
 }
 function preload() {
@@ -44,18 +44,5 @@ function draw() {
         return;
     if (!window?.engine?.getActiveScene)
         return;
-    clear();
-    background(150, 230, 240);
-    /*-------------PLAYER AND LEVEL DRAWING-----------------*/
-    player.update();
-    //Early Update
-    engine.getActiveScene().earlyUpdate();
-    player.camera();
-    player.checkCollisions();
-    engine.getActiveScene().display();
-    engine.getActiveScene().customDraw(false);
-    player.display();
-    //Late Update
-    engine.getActiveScene().lateUpdate();
-    /*-------------PLAYER AND LEVEL DRAWING-----------------*/
+    engine.draw();
 }
